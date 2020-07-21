be0daa8df2e212e53e750dd682c874f4fe898222
KRAMADHARI Anusha <anusha.kramadhari@rntbci.com>
2020-07-21 12:52:03 +0530

    bgimage_added

--- FiveFeetApp-frontend/src/app/app-routing.module.ts
+++ FiveFeetApp-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { UserHomeComponent } from './user-home/user-home.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserHomeComponent},
+  {path: 'signup', component: UserSignupComponent},
+  {path: 'login', component: UserLoginComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FiveFeetApp-frontend/src/app/app.module.ts
+++ FiveFeetApp-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { UserHomeComponent } from './user-home/user-home.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignupComponent,
+    UserHomeComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
